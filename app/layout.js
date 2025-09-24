04ff20f187bf692df63ebca558d7e1944faf091d
Usman Eletu <usmaneletu2@gmail.com>
2025-09-24 21:58:25 +0100

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,11 @@
-import { notFound } from 'next/navigation';
 import MainHeader from '../components/main-header/main-header';
 import MainHeaderBackground from '../components/main-header/main-header-background';
-import { getMeal } from '../lib/meals';
 import './globals.css';
 
-export async function generateMetadata({params}) {
-  const meal = getMeal(params.mealSlug);
-
-  if (!meal) {
-    notFound();
-  }
-  
-  return {
-    title: meal.title,
-    description: meal.summary,
-  }
-}
+export const metadata = {
+  title: 'NextLevel Food',
+  description: 'Delicious meals, shared by a food-loving community.',
+};
 
 export default function RootLayout({ children }) {
   return (
